d4f8df6a28f65f6ae7253d0a93795c0519f03b94
ThuySeven23 <huuthuytrann3004@gmail.com>
2025-02-02 22:20:24 +0700

    api refresh-token successfully

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,25 +3,32 @@ import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dtos/register-user.dto';
 import { LoginUserDto } from './dtos/login-user.dto';
 import { AuthGuard } from './auth.guard';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
+import { Public } from 'src/commons/decorators/public.decorator';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @UseGuards(AuthGuard)
-  @Get()
-  test() {
-    return {
-      message: 'Oke',
-    };
-  }
 
+  @Public()
   @Post('/register')
   registerUser(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.registerUser(registerUserDto);
   }
-
+  
+  @Public()
   @Post('/login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.loginUser(loginUserDto);
   }
+
+  @Public()
+  @Post('/refresh')
+  refresh(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refresh(refreshTokenDto);
+  }
+  
 }
+
+
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,87 +1,206 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { RegisterUserDto } from "./dtos/register-user.dto";
-import { UserRepository } from "src/databases/repositories/user.repository";
-import * as argon2 from "argon2";
-import { LOGIN_TYPE, ROLE } from "src/commons/enums/user.enum";
-import { ApplicantRepository } from "src/databases/repositories/applicant.repository";
-import { LoginUserDto } from "./dtos/login-user.dto";
-import { JwtService } from "@nestjs/jwt";
-import { ConfigService } from "@nestjs/config";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { RegisterUserDto } from './dtos/register-user.dto';
+import { UserRepository } from 'src/databases/repositories/user.repository';
+import * as argon2 from 'argon2';
+import { LOGIN_TYPE, ROLE } from 'src/commons/enums/user.enum';
+import { ApplicantRepository } from 'src/databases/repositories/applicant.repository';
+import { LoginUserDto } from './dtos/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
+import { User } from 'src/databases/entities/user.entity';
+import { LoginGoogleDto } from './dtos/login-google.dto';
+import { OAuth2Client } from 'google-auth-library';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly userRepository: UserRepository,
-        private readonly applicantRepository: ApplicantRepository,
-        private readonly jwtService: JwtService,
-        private readonly configService: ConfigService
-    ) {}
-    async registerUser(registerUserDto: RegisterUserDto) {
-        const { username, password, email } = registerUserDto;
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly applicantRepository: ApplicantRepository,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
-        // Check email exists
-        const userExist = await this.userRepository.findOneBy({ email }); 
-        if (userExist) {
-            throw new HttpException('Email already exists', HttpStatus.BAD_REQUEST);
-        }
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { username, password, email } = registerUserDto;
 
-        // Hash password of user
-        const hashPassword = await argon2.hash(password);
-
-        // Create new user and applicant record to save to database
-        const newUser = await this.userRepository.save({
-            username,
-            email,
-            password: hashPassword,
-            loginType: LOGIN_TYPE.EMAIL,
-            role: ROLE.APPLICANT,
-        });
-        await this.applicantRepository.save({
-            userId: newUser.id
-        });
-
-        return {
-            message: 'User registered successfully',
-            user: newUser,
-        };
+    // Check email exists
+    const userExist = await this.userRepository.findOneBy({ email });
+    if (userExist) {
+      throw new HttpException('Email already exists', HttpStatus.BAD_REQUEST);
     }
 
-    async loginUser(loginUserDto: LoginUserDto) {
-        const { email, password } = loginUserDto;
+    // Hash password of user
+    const hashPassword = await argon2.hash(password);
 
-        // Check if user email exists
-        const userExistByEmail = await this.userRepository.findOneBy({ email });
-        if (!userExistByEmail) {
-            throw new HttpException('Incorrect email or password', HttpStatus.UNAUTHORIZED);
-        }
+    // Create new user and applicant record to save to database
+    const newUser = await this.userRepository.save({
+      username,
+      email,
+      password: hashPassword,
+      loginType: LOGIN_TYPE.EMAIL,
+      role: ROLE.APPLICANT,
+    });
+    await this.applicantRepository.save({
+      userId: newUser.id,
+    });
 
-        // Compare password from user to database password
-        const isPasswordValid = await argon2.verify(userExistByEmail.password, password);
-        if (!isPasswordValid) {
-            throw new HttpException('Incorrect email or password', HttpStatus.UNAUTHORIZED);
-        }
+    return {
+      message: 'User registered successfully',
+      user: newUser,
+    };
+  }
 
-        // Create token for user
-        const payload = { 
-            id: userExistByEmail.id, 
-            username: userExistByEmail.username, 
-            loginType: userExistByEmail.loginType, 
-            role: userExistByEmail.role, 
-        };
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
 
-        // Generate JWT token for user
-        const access_token = await this.jwtService.signAsync(payload, {
-            secret: this.configService.get('jwtAuth').jwtTokenSecret,
-            expiresIn: this.configService.get('jwtAuth').expiresIn,
-        });
-
-        return {
-            message: 'Login successfully',
-            result: {
-                access_token
-            }
-        };
+    // Check if user email exists
+    const userRecord = await this.userRepository.findOneBy({ email });
+    if (!userRecord) {
+      throw new HttpException(
+        'Incorrect email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
 
+    // Compare password from user to database password
+    const isPasswordValid = await argon2.verify(userRecord.password, password);
+    if (!isPasswordValid) {
+      throw new HttpException(
+        'Incorrect email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
 
+    // Generate JWT access token
+    const payload = await this.getPayload(userRecord);
+    const { accessToken, refreshToken } = await this.signToken(payload);
+
+    return {
+      message: 'Login successfully',
+      result: {
+        accessToken,
+        refreshToken,
+      },
+    };
+  }
+
+  async refresh(refreshTokenDto: RefreshTokenDto) {
+    const { refreshToken: oldRefreshToken } = refreshTokenDto;
+
+    //Verify refresh token
+    const payloadRefreshToken = await this.jwtService.verifyAsync(
+      oldRefreshToken,
+      {
+        secret: this.configService.get('jwtAuth').jwtRefreshTokenSecret,
+      },
+    );
+    if (!payloadRefreshToken) {
+      throw new HttpException('Invalid refresh token', HttpStatus.UNAUTHORIZED);
+    }
+
+    // Check if user exists in database by id from refresh token payload
+    const userRecord = await this.userRepository.findOneBy({
+      id: payloadRefreshToken.id,
+    });
+    if (!userRecord) {
+      throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
+    }
+
+    // Generate JWT access token
+    const payload = await this.getPayload(userRecord);
+    const { accessToken, refreshToken } = await this.signToken(payload);
+
+    return {
+      message: 'Refresh token successfully',
+      result: {
+        accessToken,
+        refreshToken,
+      },
+    };
+  }
+
+  async getPayload(user: User) {
+    return {
+      id: user.id,
+      username: user.username,
+      loginType: user.loginType,
+      role: user.role,
+    };
+  }
+
+  async signToken(payloadAccess) {
+    const payloadRefresh = {
+      id: payloadAccess.id,
+    };
+
+    const accessToken = await this.jwtService.signAsync(payloadAccess, {
+      secret: this.configService.get('jwtAuth').jwtTokenSecret,
+      expiresIn: '15m',
+    });
+
+    const refreshToken = await this.jwtService.signAsync(payloadRefresh, {
+      secret: this.configService.get('jwtAuth').jwtRefreshTokenSecret,
+      expiresIn: '7d',
+    });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async loginGoogle(body: LoginGoogleDto) {
+    const { token } = body;
+    const ggClientId = this.configService.get('google').clientId;
+    const ggClientSecret = this.configService.get('google').clientSecret;
+    const oAuthClient = new OAuth2Client(ggClientId, ggClientSecret);
+    const ggLoginTicket = await oAuthClient.verifyIdToken({
+      idToken: token,
+      audience: ggClientId,
+    });
+
+    const { email_verified, email, name } = (await ggLoginTicket).getPayload();
+    if (!email_verified) {
+      throw new HttpException(
+        'Email is not verified' + email,
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    let userRecord = await this.userRepository.findOneBy({
+      email,
+      loginType: LOGIN_TYPE.GOOGLE,
+    });
+
+    if (userRecord && userRecord.loginType === LOGIN_TYPE.EMAIL) {
+      throw new HttpException(
+        'Email use to login:' + email,
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    if (!userRecord) {
+      userRecord = await this.userRepository.save({
+        email,
+        username: name,
+        loginType: LOGIN_TYPE.GOOGLE,
+      });
+
+      await this.applicantRepository.save({
+        userId: userRecord.id,
+      });
+    }
+
+    const payload = await this.getPayload(userRecord);
+    const { accessToken, refreshToken: newRefreshToken } =
+      await this.signToken(payload);
+    return {
+      message: 'Login with google successfully',
+      result: {
+        accessToken,
+        refreshToken: newRefreshToken,
+      },
+    };
+  }
 }
